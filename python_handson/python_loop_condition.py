4e45b5abe2e32c147ed6316aed2a1f2770960de5
Aniket Gole <roshan3133@gmail.com>
2016-05-14 16:14:22 +0530

    python loop and condition examples

---
+++ python_handson/python_loop_condition.py
@@ -0,0 +1,18 @@
+#1. while loop
+counter = 1
+while counter <= 10: 
+  print "hi"     #till the condition false it will print 
+  counter += 1
+  
+#2. for loop
+for i in range(10):
+  print i
+
+#3. if else
+for a in range(11):
+  if a == 10:
+    print "a is 10 now"
+  elif a == 9:
+    print "a is 9 now"
+  else:
+    print "a is nither 9 or 10"
