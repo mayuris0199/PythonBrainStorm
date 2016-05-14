1ad36c5f1197d8e425e327edd461e5c02578d14f
Aniket Gole <roshan3133@gmail.com>
2016-05-14 15:00:36 +0530

    python inbuilt functions

--- python_handson/python_inbuilt_functions.py
+++ python_handson/python_inbuilt_functions.py
@@ -1,12 +1,44 @@
-#1. Enumerate
+# 1. Enumerate
 a = ["a", "b", "c"]
 print list(enumerate(a))
 #output
 #[(0, 'a'), (1, 'b'), (2, 'c')]
 
-#2. exec : How to run piece of code at runtime
+# 2. exec 
+# How to run piece of code at runtime
 exec("x = 1")
 print x
 
 #3. eval 
 print eval("2+3")
+
+#4. break - Terminates the loop statement and transfers execution to the statement immediately following the loop.
+#5. continue - Causes the loop to skip the remainder of its body and immediately retest its condition prior to reiterating.
+#6. pass - The pass statement in Python is used when a statement is required syntactically but you do not want any command or code to execute.
+#7. choice() - Returns a random item from a list, tuple, or string.
+#8. randrange ([start,] stop [,step]) − returns a randomly selected element from
+#9. random() − returns a random float r, such that 0 is less than or equal to r and r is less than 1.
+#10. shuffle(lst) − Randomizes the items of a list in place. Returns None
+#11. capitalize() − Capitalizes first letter of string.
+#12. isalnum() − Returns true if string has at least 1 character and all characters are alphanumeric and false otherwise.
+#13. isdigit() − Returns true if string contains only digits and false otherwise.
+#14. islower() − Returns true if string has at least 1 cased character and all cased characters are in lowercase and false otherwise.
+#15. isupper() − Returns true if string has at least one cased character and all cased characters are in uppercase and false otherwise.
+#16. isnumeric() − Returns true if a unicode string contains only numeric characters and false otherwise.
+#17. isspace() − Returns true if string contains only whitespace characters and false otherwise.
+#18. istitle() − Returns true if string is properly "titlecased" and false otherwise.
+#19. join(seq) − Merges (concatenates) the string representations of elements in sequence seq into a string, with separator string.
+#20. len(string) − Returns the length of the string.
+#21. ljust(width[, fillchar]) − Returns a space-padded string with the original string left-justified to a total of width columns.
+#22. lower() − Converts all uppercase letters in string to lowercase.
+#23  upper() − Converts all lowercase letters in string to uppercase.
+#24. lstrip() − Removes all leading whitespace in string.
+#25. rstrip() − Removes all trailing whitespace in string.
+#26. strip() − Removes all leading & trailing whitespace in string.
+#27. max(str) − Returns the max alphabetical character from the string str.
+#28. min(str) − Returns the min alphabetical character from the string str.
+#29. swapcase() − Inverts case for all letters in string.
+#30. title() − Returns "titlecased" version of string, that is, all words begin with uppercase and the rest are lowercase.
+#31. isdecimal() − Returns true if a unicode string contains only decimal characters and false otherwise.\
+#32. cmp(list1, list2) − Compares elements of both lists.
+#33. replace(old, new [, max]) − Replaces all occurrences of old in string with new or at most max occurrences if max given.
